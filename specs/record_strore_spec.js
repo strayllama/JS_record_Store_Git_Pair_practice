33408394e773ba446857020b9cad9f2d2febf50b
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-04-12 15:30:03 +0100

    Record collection added and tested

--- specs/record_strore_spec.js
+++ specs/record_strore_spec.js
@@ -1,12 +1,19 @@
 const assert = require('assert')
 const RecordStore = require('../models/record_store.js')
+const Record = require('../models/record.js')
 
 describe('RecordStore', function() {
 
   let record_store;
+  let record1;
+  let record2;
+  let record3;
 
   beforeEach(function(){
-    record_store = new RecordStore('Madrid Rock', 'Madrid')
+    record_store = new RecordStore('Madrid Rock', 'Madrid');
+    record1 = new Record('Katie Tunstall', 'Black Horse and the Cherry Tree', 'Indie', 200);
+    record2 = new Record('Post Malone', 'Stoney', 'Trap', 150);
+    record3 = new Record('Postal Service', 'The District Sleeps Alone Tonight', 'Indie', 150);
   });
 
   it ('should have a name', function(){
@@ -28,5 +35,13 @@ describe('RecordStore', function() {
     const result = record_store.balance;
     assert.strictEqual(result, 0)
   });
+
+  it ('cand add records to inventory', function(){
+    record_store.inventory.push(record1);
+    record_store.inventory.push(record2);
+    record_store.inventory.push(record3);
+    const result = record_store.inventory.length;
+    assert.strictEqual(result, 3);
+  })
 
 });
